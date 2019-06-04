2d1103a5b635bd4edc13ef9061b44276644793f4
An Nadein <an.nadein@jpmorgan.com>
2019-06-04 15:44:36 -0400

    SANDBONI-845 PoC for JUnit 845 fixed logging

--- src/main/java/com/jpmorgan/cib/coreeng/ste/java_lint_assert/LintAssertMethodVisitor.java
+++ src/main/java/com/jpmorgan/cib/coreeng/ste/java_lint_assert/LintAssertMethodVisitor.java
@@ -2,6 +2,7 @@ package com.jpmorgan.cib.coreeng.ste.java_lint_assert;
 
 import org.objectweb.asm.AnnotationVisitor;
 import org.objectweb.asm.MethodVisitor;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,18 +15,18 @@ public class LintAssertMethodVisitor extends MethodVisitor {
     public LintAssertMethodVisitor(int version, MethodVisitor mv, String methodName) {
         super(version, mv);
         this.methodName = methodName;
-        log.warn("version=" + version +", mv=" + mv + ", methodName=" + methodName);
+        log.debug("version=" + version +", mv=" + mv + ", methodName=" + methodName);
     }
 
     @Override
     public AnnotationVisitor visitAnnotationDefault() {
-        log.warn("visitAnnotationDefault");
+        log.debug("visitAnnotationDefault");
         return super.visitAnnotationDefault();
     }
 
     @Override
     public AnnotationVisitor visitAnnotation(String descriptor, boolean visible) {
-        log.warn("visitAnnotation");
+        log.debug("visitAnnotation");
         return super.visitAnnotation(descriptor, visible);
     }
 }
