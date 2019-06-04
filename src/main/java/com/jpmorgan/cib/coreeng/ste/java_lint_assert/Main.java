8c982514f7b33a3dbe6f4fc2da610a67ea0dedf6
An Nadein <an.nadein@jpmorgan.com>
2019-06-03 22:06:21 -0400

    SANDBONI-845: PoC for JUnit

---
+++ src/main/java/com/jpmorgan/cib/coreeng/ste/java_lint_assert/Main.java
@@ -0,0 +1,8 @@
+package com.jpmorgan.cib.coreeng.ste.java_lint_assert;
+
+public class Main {
+
+     public static void main(String[] args){
+
+     }
+}
---
+++ src/test/java/com/jpmorgan/cib/coreeng/ste/java_lint_assert/MainTest.java
@@ -0,0 +1,15 @@
+package com.jpmorgan.cib.coreeng.ste.java_lint_assert;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest{
+
+//    @Test
+//    void testAssertAbsence() {
+//    }
+
+    @Test
+    void justAnExample() {
+        System.out.println("This test method should be run");
+    }
+}
