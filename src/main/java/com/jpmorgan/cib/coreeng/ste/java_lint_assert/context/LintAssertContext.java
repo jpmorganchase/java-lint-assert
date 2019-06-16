8f119e6bd07ab14cec2126618c3a3be3608f1158
An Nadein <an.nadein@jpmorgan.com>
2019-06-15 21:37:37 -0400

    SANDBONI-845 PoC for JUnit 845 fixed

--- src/main/java/com/jpmorgan/cib/coreeng/ste/java_lint_assert/context/LintAssertContext.java
+++ src/main/java/com/jpmorgan/cib/coreeng/ste/java_lint_assert/context/LintAssertContext.java
@@ -1,6 +1,5 @@
 package com.jpmorgan.cib.coreeng.ste.java_lint_assert.context;
 
-import com.jpmorgan.cib.coreeng.ste.java_lint_assert.visitor.LintAssertMethodVisitor;
 import org.javatuples.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,7 +15,7 @@ public class LintAssertContext {
 
     private  TestMethodContext testMethodContext;
 
-    private static Logger log = LoggerFactory.getLogger(LintAssertMethodVisitor.class);
+    private static Logger log = LoggerFactory.getLogger(LintAssertContext.class);
 
     public LintAssertContext(int asmVersion) {
         this.asmVersion = asmVersion;
@@ -25,8 +24,7 @@ public class LintAssertContext {
     }
 
     public void recordAssert(int atLineNumber, String assertMethodName) {
-        log.debug(this.testMethodContext.fileName + "==" + this.testMethodContext.methodName + ", assertMethodName=" + assertMethodName);
-
+//        log.debug(this.testMethodContext.fileName + "==" + this.testMethodContext.methodName + ", assertMethodName=" + assertMethodName);
         this.testMethodContext.assertMethodsAtLineNumbers.add(new Pair<>(atLineNumber, assertMethodName));
     }
 
@@ -43,15 +41,6 @@ public class LintAssertContext {
     public void with(String descriptor, boolean visible) {
         this.testMethodContext.methodDescriptor = descriptor;
         this.testMethodContext.visible = visible;
-    }
-
-    @Override
-    public String toString() {
-        return "LintAssertContext{" +
-                "asmVersion=" + asmVersion +
-                ", testMethodsContext=" + testMethodsContext +
-                ", testMethodContext=" + testMethodContext +
-                '}';
     }
 
     public void setMethodName(String name) {
@@ -73,6 +62,15 @@ public class LintAssertContext {
     public Set<TestMethodContext> getTestMethodsContext(){return testMethodsContext;}
 
     public void setFileName(String fileName){this.testMethodContext.fileName = fileName;}
+
+    @Override
+    public String toString() {
+        return "LintAssertContext{" +
+                "asmVersion=" + asmVersion +
+                ", testMethodsContext=" + testMethodsContext +
+                ", testMethodContext=" + testMethodContext +
+                '}';
+    }
 
 }
 
