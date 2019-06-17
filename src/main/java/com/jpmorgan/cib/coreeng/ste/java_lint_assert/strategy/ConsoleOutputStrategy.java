32dae76aab7f62dc6cd20445cbd793da1b5976e2
An Nadein <an.nadein@jpmorgan.com>
2019-06-17 16:17:04 -0400

    SANDBONI-845 PoC for JUnit 845 minor refactoring

--- src/main/java/com/jpmorgan/cib/coreeng/ste/java_lint_assert/strategy/ConsoleOutputStrategy.java
+++ src/main/java/com/jpmorgan/cib/coreeng/ste/java_lint_assert/strategy/ConsoleOutputStrategy.java
@@ -11,39 +11,31 @@ public class ConsoleOutputStrategy {
 
     public static final String PIPE = "|";
     public static final String LINE_SEPARATOR = System.getProperty("line.separator");
-    public final int PADDING = 4;
-    public final char SEPARATOR_CHAR = '-';
+    public static final int PADDING = 4;
+    public static final char SEPARATOR_CHAR = '-';
+    public static final Collection<String> headers = Arrays.asList("Package", "Test file name", "Test method name", "# asserts");
 
-    private final Set<TestMethodContext> contexts;
-    private final Collection<String> headers;
     private ArrayList<Integer> maxLength;
+    private final Set<TestMethodContext> contexts;
 
-    public ConsoleOutputStrategy(Set<TestMethodContext> testMethodContexts){
-        this( Arrays.asList("Package",
-                                "Test file name",
-                                "Test method name",
-                                "# asserts"), testMethodContexts);
-    }
+    public ConsoleOutputStrategy(Set<TestMethodContext> testMethodContexts) {
+        this.contexts = testMethodContexts;
+        this.maxLength = new ArrayList<>();
 
-    public ConsoleOutputStrategy(Collection<String> headers, Set<TestMethodContext> testMethodsContext) {
-        this.headers = headers;
-        this.contexts = testMethodsContext;
+        calculateEachCellWidth();
     }
 
     public String render() {
-        StringBuilder content = this.renderHeaders();
+        StringBuilder content = this.renderHeader();
         return content.append(this.renderBody()).toString();
     }
 
-    StringBuilder renderHeaders() {
-        StringBuilder sb = new StringBuilder();
-
-        this.maxLength = new ArrayList<>();
+    private void calculateEachCellWidth() {
+        //the initial cell width must fit the header's label, so that a 'no tests' table is formatted
         for (String header : headers) {
             maxLength.add(header.length());
         }
 
-//        int i = 0;
         for (TestMethodContext context : contexts) {
             if (maxLength.get(0) < context.getPackageName().length())
                 maxLength.set(0, context.getPackageName().length());
@@ -53,9 +45,11 @@ public class ConsoleOutputStrategy {
                 maxLength.set(2, context.getMethodName().length());
             if (maxLength.get(3) < context.getAssertMethodsAtLineNumbers().size())
                 maxLength.set(3, context.getAssertMethodsAtLineNumbers().size());
-//            i %= headers.size();
         }
+    }
 
+    protected StringBuilder renderHeader() {
+        final StringBuilder sb = new StringBuilder();
 
         renderSeparatorRow(sb);
         sb.append(LINE_SEPARATOR);
@@ -64,30 +58,55 @@ public class ConsoleOutputStrategy {
         renderSeparatorRow(sb);
         sb.append(LINE_SEPARATOR);
 
+        return sb;
+    }
+
+    protected StringBuilder renderBody() {
+
+        final StringBuilder sb = new StringBuilder();
+
         for (TestMethodContext context : contexts) {
             renderRow(context, sb);
             sb.append(PIPE);
             sb.append(LINE_SEPARATOR);
         }
-
         return sb;
     }
 
-    void renderRow(TestMethodContext context, StringBuilder sb) {
+    protected void renderHeaderRow(StringBuilder sb) {
+        int i = 0;
+        for (String header : headers) {
+            renderCell(sb, header, i);
+            i++;
+        }
+        sb.append(PIPE);
+    }
+
+    protected void renderSeparatorRow(StringBuilder sb) {
+        for (Integer cellLengh : maxLength) {
+            sb.append(PIPE);
+            for (int j = 0; j < cellLengh + (PADDING * 2); j++) {
+                sb.append(SEPARATOR_CHAR);
+            }
+        }
+        sb.append(PIPE);
+    }
+
+    protected void renderRow(TestMethodContext context, StringBuilder sb) {
         renderCell(sb, context.getPackageName(), 0);
         renderCell(sb, context.getFileName(), 1);
         renderCell(sb, context.getMethodName(), 2);
         renderCell(sb, context.getAssertMethodsAtLineNumbers().size(), 3);
     }
 
-    private void renderCell(StringBuilder sb, Object text, int cell) {
+    protected void renderCell(StringBuilder sb, Object text, int cell) {
         sb.append(PIPE);
         sb.append(pad(maxLength.get(cell), text.toString(), false));
         sb.append(text);
         sb.append(pad(maxLength.get(cell), text.toString(), true));
     }
 
-    String pad(int cellWidth, String cellValue, boolean isRightHandSide) {
+    protected String pad(int cellWidth, String cellValue, boolean isRightHandSide) {
         StringBuilder padder = new StringBuilder();
 
         int emptySpaces = cellWidth - cellValue.length();
@@ -101,26 +120,4 @@ public class ConsoleOutputStrategy {
         return padder.toString();
     }
 
-    void renderHeaderRow(StringBuilder sb) {
-        int i = 0;
-        for (String header : headers) {
-            renderCell(sb, header, i);
-            i++;
-        }
-        sb.append(PIPE);
-    }
-
-    void renderSeparatorRow(StringBuilder sb) {
-        for (Integer cellLengh : maxLength) {
-            sb.append(PIPE);
-            for (int j = 0; j < cellLengh + (PADDING * 2); j++) {
-                sb.append(SEPARATOR_CHAR);
-            }
-        }
-        sb.append(PIPE);
-    }
-
-    StringBuilder renderBody() {
-        return new StringBuilder();
-    }
 }
