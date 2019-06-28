89f648df6d366028f0de2cd3b6a6812db1bed36b
An Nadein <an.nadein@jpmorgan.com>
2019-06-28 12:51:36 -0400

    SANDBONI-896 Run as a Maven pugin 896 + renamed packages, removed slf4j

--- src/main/java/com/lint/_assert/util/PropertiesLoader.java
+++ src/main/java/com/lint/_assert/util/PropertiesLoader.java
@@ -1,4 +1,4 @@
-package com.jpmorgan.cib.coreeng.ste.java_lint_assert.util;
+package com.lint._assert.util;
 
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -6,13 +6,14 @@ import org.json.simple.parser.ParseException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public final class PropertiesLoader {
 
     public static Object load(String pathToPropertiesFile) throws IOException, ParseException {
         InputStream reader = PropertiesLoader.class.getResourceAsStream(pathToPropertiesFile);
         JSONParser jsonParser = new JSONParser();
-        return jsonParser.parse(new InputStreamReader(reader));
+        return jsonParser.parse(new InputStreamReader(reader, StandardCharsets.UTF_8));
     }
 
 }
