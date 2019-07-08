12fc2dcae6b9692348a0a268fcc6b8af9e3d0a26
An Nadein <an.nadein@jpmorgan.com>
2019-07-08 15:53:33 -0400

    SANDBONI-896 Run as a Gradle pugin fixing jules build, again...

--- plugin/src/main/java/com/lint/azzert/LintAssertTask.java
+++ plugin/src/main/java/com/lint/azzert/LintAssertTask.java
@@ -1,19 +1,13 @@
 package com.lint.azzert;
 
 import com.lint.azzert.context.Context;
-import com.lint.azzert.strategy.ConsoleOutputStrategy;
-import com.lint.azzert.util.TestClassFinder;
 import com.lint.azzert.visitor.LintAssertClassVisitor;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
 import org.json.simple.parser.ParseException;
-import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassVisitor;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
 
 public class LintAssertTask extends DefaultTask {
 
@@ -34,15 +28,15 @@ public class LintAssertTask extends DefaultTask {
         Context context = new ContextBuilder().build();
         final ClassVisitor classVisitor = new LintAssertClassVisitor(context);
 
-        List<File> classFiles = TestClassFinder.getClasses(this.classPath, this.getClass().getPackage().getName());
-        for (File classFile : classFiles) {
-            final String classPath = TestClassFinder.buildClassFilePath(classFile.getPath());
-            InputStream inputStream = LintAssertTask.class.getResourceAsStream(classPath);
-            ClassReader classReader = new ClassReader(inputStream);
-            classReader.accept(classVisitor, 0);
-        }
-
-        String output = new ConsoleOutputStrategy(context.getTestMethodsContext()).render();
-        System.out.println(output);
+//        List<File> classFiles = TestClassFinder.getClasses(this.classPath, this.getClass().getPackage().getName());
+//        for (File classFile : classFiles) {
+//            final String classPath = TestClassFinder.buildClassFilePath(classFile.getPath());
+//            InputStream inputStream = LintAssertTask.class.getResourceAsStream(classPath);
+//            ClassReader classReader = new ClassReader(inputStream);
+//            classReader.accept(classVisitor, 0);
+//        }
+//
+//        String output = new ConsoleOutputStrategy(context.getTestMethodsContext()).render();
+//        System.out.println(output);
     }
 }
