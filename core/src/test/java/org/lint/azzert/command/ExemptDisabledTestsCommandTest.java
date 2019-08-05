72d86b7b27e584cff63146aec83656a6949b65fe
An Nadein <an.nadein@jpmorgan.com>
2019-08-05 17:04:41 -0400

    SANDBONI-1004 set up CI and Code Quality for github's version + fixing the merge

--- core/src/test/java/org/lint/azzert/command/ExemptDisabledTestsCommandTest.java
+++ core/src/test/java/org/lint/azzert/command/ExemptDisabledTestsCommandTest.java
@@ -1,17 +1,31 @@
 package org.lint.azzert.command;
 
-import org.lint.azzert.context.Context;
+import org.javatuples.Pair;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.lint.azzert.ToStringAssertProcessor;
+import org.lint.azzert.context.TestMethodContext;
+import org.lint.azzert.strategy.ToStringStrategy;
 
-import java.util.Collections;
+import java.util.Set;
+import java.util.function.Function;
 
-public class ExemptDisabledTestsCommand implements LintCommand{
+public class ExemptDisabledTestsCommandTest {
 
-    @Override
-    //remove all disabled tests
-    public Void execute(final Context context){
-        context.getMethodContexts().removeIf(
-                method -> ! Collections.disjoint(context.getSupportedExemptApis(), method.getAnnotations())
-        );
-        return null;
+    @Test
+    void test() {
+        Set<TestMethodContext> methods = new ToStringAssertProcessor(
+                null, Pair.with(ExemptDisabledTestsCommandTest.class.getPackage().getName(), false)
+        ).process();
+
+        System.out.println(new ToStringStrategy(methods).render());
+        methods.forEach(System.out::println);
+
+        final Function<String, Long> assertsInMethod = str -> methods.stream().filter(m-> m.getMethodName().equals(str)
+                && m.getFileName().contains("ExemptDisabledTestsCommandTest")).count();
+
+        Assertions.assertEquals(0, assertsInMethod.apply("youWouldThinkIAmATestButIAmNot"), "Should've excluded 'youWouldThinkIAmATestButIAmNot' annotated *only* with @Disabled");
+        Assertions.assertEquals(0, assertsInMethod.apply("iAmDisabled"), "Should've excluded 'iAmDisabled' annotated with @Disabled");
+        Assertions.assertEquals(1, assertsInMethod.apply("test"), "Should've included 'test' annotated with @Test");
     }
 }
