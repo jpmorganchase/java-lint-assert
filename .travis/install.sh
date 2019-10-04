272bb5ce0a75ad49eb9e7532be653a2b9e3a2b8d
AN <matrixpooh@ANs-iMac.local>
2019-10-03 20:18:07 -0400

    fixing '-iv undefined'

--- .travis/install.sh
+++ .travis/install.sh
@@ -2,5 +2,5 @@
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ];
 then
-    openssl aes-256-cbc -K $encrypted_833b66024efe_key -iv $encrypted_833b66024efe_iv -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d -p
+    openssl aes-256-cbc -K $encrypted_833b66024efe_key -iv $encrypted_833b66024efe -in $ENCRYPTED_GPG_KEY_LOCATION -out $GPG_KEY_LOCATION -d -p
 fi
