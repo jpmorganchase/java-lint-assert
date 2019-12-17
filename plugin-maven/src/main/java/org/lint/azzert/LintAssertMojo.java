035e59dcca0701a74055efb2b17d20bef2b15c1f
An Nadein <an.nadein@jpmorgan.com>
2019-12-17 13:57:16 -0500

    https://github.com/jpmorganchase/java-lint-assert/issues/11: run linter as Maven plugin : refactoring + client-maven

--- plugin-maven/src/main/java/org/lint/azzert/LintAssertMojo.java
+++ plugin-maven/src/main/java/org/lint/azzert/LintAssertMojo.java
@@ -3,6 +3,11 @@ package org.lint.azzert;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Mojo;
 
+/*
+Test stand-alone:
+mvn install
+mvn lint-assert:lint-assert
+ */
 @Mojo(name = "lint-assert")
 public class LintAssertMojo extends AbstractMojo {
 
