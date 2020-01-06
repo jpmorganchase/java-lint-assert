1ab7628eef3ffbd057ffd7f54acb2ddcbdc207c4
An Nadein <an.nadein@jpmorgan.com>
2020-01-06 13:50:26 -0500

    fixing travis build

--- plugin-maven/src/main/java/org/lint/azzert/LintAssertMojo.java
+++ plugin-maven/src/main/java/org/lint/azzert/LintAssertMojo.java
@@ -11,7 +11,26 @@ mvn lint-assert:lint-assert
 @Mojo(name = "lint-assert")
 public class LintAssertMojo extends AbstractMojo {
 
+    @Parameter( property = "packageName")
+    private String packageName;
+
+    @Parameter( property = "includeClasspathJars", defaultValue = false)
+    private boolean includeClasspathJars;
+
+    @Parameter( property = "verbose", defaultValue = false)
+    private boolean verbose;
+
     public void execute() {
-        getLog().info("Hello from LintAssertMojo");
+        getLog().info("Hello from LintAssertMojo!");
+        getLog().info(toString());
+    }
+
+    @Override
+    public String toString() {
+        return "LintAssertMojo{" +
+                "packageName='" + packageName + '\'' +
+                ", includeClasspathJars=" + includeClasspathJars +
+                ", verbose=" + verbose +
+                '}';
     }
 }
