ced5385708aa8d9dbb4377995446dbb48bfac375
An Nadein <an.nadein@jpmorgan.com>
2020-01-06 22:02:56 -0500

    https://github.com/jpmorganchase/java-lint-assert/issues/11: run linter as Maven plugin : works with 1 defect: the classpath dependency that contains additional tests must be included at the 'compile' scope in the pom.

--- plugin-maven/src/main/java/org/lint/azzert/LintAssertMojo.java
+++ plugin-maven/src/main/java/org/lint/azzert/LintAssertMojo.java
@@ -1,8 +1,25 @@
 package org.lint.azzert;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.descriptor.PluginDescriptor;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.classworlds.realm.ClassRealm;
+import org.lint.azzert.context.MethodMetadata;
+import org.lint.azzert.processor.LintAssertBuildParameters;
+import org.lint.azzert.processor.LintAssertProcessor;
+import org.lint.azzert.strategy.output.ToStringStrategy;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
 
 /*
 Test stand-alone:
@@ -11,18 +28,52 @@ mvn install lint-assert:lint-assert
 @Mojo(name = "lint-assert")
 public class LintAssertMojo extends AbstractMojo {
 
-    @Parameter( property = "packageName")
+    @Parameter( property = "packageName", required = false)
     private String packageName;
 
-    @Parameter( property = "includeClasspathJars", defaultValue = "false")
+    @Parameter( property = "includeClasspathJars", defaultValue = "false", required = false)
     private boolean includeClasspathJars;
 
-    @Parameter( property = "verbose", defaultValue = "false")
+    @Parameter( property = "verbose", defaultValue = "false", required = false)
     private boolean verbose;
 
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    protected MavenProject project;
+
     public void execute() {
-        getLog().info("Hello from LintAssertMojo!;;" + this.toString());
-        getLog().info(this.toString());
+        try {
+            Set<MethodMetadata> methodMetadata = new LintAssertProcessor(getUrlClassLoader(),
+                    new LintAssertBuildParameters(packageName, verbose, includeClasspathJars)).process();
+            String result = new ToStringStrategy(methodMetadata).render();
+            getLog().info(result);
+        } catch (Exception e) {
+            getLog().error("Failed to lint", e);
+        }
+    }
+
+    URLClassLoader getUrlClassLoader() throws DependencyResolutionRequiredException {
+        final List<String> testDir = this.project.getTestClasspathElements();
+
+        URL[] compiledTestsDirs = testDir.stream()
+                .map(File::new)
+                .map(File::toURI)
+                .map(u -> {
+                            try {
+                                return u.toURL();
+                            } catch (MalformedURLException ignore) {
+                                return "";
+                            }
+                        }
+                )
+                .toArray(URL[]::new);
+
+        if (includeClasspathJars) {
+            final PluginDescriptor pluginDescriptor = (PluginDescriptor) getPluginContext().get("pluginDescriptor");
+            final ClassRealm classRealm = pluginDescriptor.getClassRealm();
+            compiledTestsDirs = ArrayUtils.addAll(compiledTestsDirs,classRealm.getURLs() );
+        }
+
+        return new URLClassLoader(compiledTestsDirs, null);
     }
 
     @Override
---
+++ plugin-maven/src/test/java/org/lint/azzert/Dummy.java
@@ -0,0 +1,13 @@
+package org.lint.azzert;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Dummy {
+
+    @Test
+    void dummy(){
+        Assertions.assertNotNull("");
+    }
+
+}
