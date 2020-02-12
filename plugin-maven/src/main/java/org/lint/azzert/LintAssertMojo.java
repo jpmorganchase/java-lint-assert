2b5ad2423992bed2906871d80643b9701f468e4b
An Nadein <an.nadein@jpmorgan.com>
2020-02-12 17:17:06 -0500

    Revert "https://github.com/jpmorganchase/java-lint-assert/issues/11: run linter as Maven plugin : WIP: broke the impl, reverted, restored to 'runnable' state, reading from classpath is still broken

--- plugin-maven/src/main/java/org/lint/azzert/LintAssertMojo.java
+++ plugin-maven/src/main/java/org/lint/azzert/LintAssertMojo.java
@@ -17,7 +17,6 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
