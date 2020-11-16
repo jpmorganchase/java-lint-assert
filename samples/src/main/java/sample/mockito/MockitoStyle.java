fc783a51cbc72249ac068ea1ac68a40736d11e5b
An Nadein <an.nadein@jpmchase.com>
2020-11-16 14:23:08 -0500

    27: Support Mockito verifications https://github.com/jpmorganchase/java-lint-assert/issues/27 init commit

---
+++ samples/src/main/java/sample/mockito/MockitoStyle.java
@@ -0,0 +1,25 @@
+package sample.mockito;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+public class MockitoStyle {
+
+    @Test
+    void test() {
+        // mock creation
+        List mockedList = mock(List.class);
+
+        // using mock object - it does not throw any "unexpected interaction" exception
+        mockedList.add("one");
+        mockedList.clear();
+
+        // selective, explicit, highly readable verification
+        verify(mockedList).add("one");
+        verify(mockedList).clear();
+    }
+}
